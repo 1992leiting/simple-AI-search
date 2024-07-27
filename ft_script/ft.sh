ed8386dfc514680ca20fd60d65828b4e55a32cc7
leiting <leiting.up@gmail.com>
2024-07-27 14:36:34 +0000

    update ft scipt

--- ft_script/ft.sh
+++ ft_script/ft.sh
@@ -1,8 +1,8 @@
 CUDA_VISIBLE_DEVICES=0,1 \
 swift sft \
     --model_type qwen2-7b-instruct \
-    --dataset /edisk/projects/swift_ft/ft_data.json \
-    --num_train_epochs 20 \
+    --dataset ft_data.json \
+    --num_train_epochs 10 \
     --save_steps 10\
     --sft_type lora \
     --output_dir output
